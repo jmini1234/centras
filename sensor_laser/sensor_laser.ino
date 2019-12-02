db29779421279f1ef39f00e17c4bbc5752bf680a
sypark9646 <sypark9646@gmail.com>
2019-12-02 13:41:46 +0900

    .

--- sensor_laser/sensor_laser.ino
+++
@@ -1,9 +0,0 @@
-void setup ()
-{
-  Serial.begin(9600);
-  pinMode (13, OUTPUT); // define the digital output interface 13 feet
-}
-void loop () {
-   digitalWrite (13, HIGH); // open the laser head
-   delay(1000);     // 1초 동안 딜레이.
-}
