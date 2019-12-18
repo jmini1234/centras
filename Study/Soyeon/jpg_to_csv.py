7caaf474bf6284f9af33f91c0365aae8d5e02dc1
soyeon Park <sypark9646@gmail.com>
2019-12-18 16:58:15 +0900

    Update jpg_to_csv.py

--- Study/Soyeon/jpg_to_csv.py
+++ Study/Soyeon/jpg_to_csv.py
@@ -5,12 +5,14 @@ import cv2
 import csv
 
 img = cv2.imread('C:/Users/CSE_125-2/Desktop/images.jpg',cv2.COLOR_BGR2GRAY)     
-# pixels = np.array(img)
+pixels = np.array(img)
+print(pixels)
 # plt.imshow(pixels)
 # plt.show()
 value = np.asarray(img, dtype=np.int)
 value = value.flatten()
+print(value)
 #print(value.size) # 240*240*3
-with open("output.csv", 'a') as f:
-    writer = csv.writer(f)
+with open("output.csv", "w", encoding="UTF-8") as f:
+    writer = csv.writer(f, delimiter=";")
     writer.writerow(value)
