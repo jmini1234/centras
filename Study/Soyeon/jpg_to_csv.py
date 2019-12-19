56062b472fbf1d69fe86aac5a2e95879d2e7823e
soyeon Park <sypark9646@gmail.com>
2019-12-19 12:42:39 +0900

    Update jpg_to_csv.py

--- Study/Soyeon/jpg_to_csv.py
+++ Study/Soyeon/jpg_to_csv.py
@@ -1,18 +1,26 @@
 import numpy as np
+import cv2 ##!pip install opencv-python
+import os
+import re # 파일 이름에서 크기와 거리 추출
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import cv2
 import csv
 
-img = cv2.imread('C:/Users/CSE_125-2/Desktop/images.jpg',cv2.COLOR_BGR2GRAY)     
-pixels = np.array(img)
-print(pixels)
-# plt.imshow(pixels)
-# plt.show()
-value = np.asarray(img, dtype=np.int)
-value = value.flatten()
-print(value)
-#print(value.size) # 240*240*3
-with open("output.csv", "w", encoding="UTF-8") as f:
-    writer = csv.writer(f, delimiter=";")
-    writer.writerow(value)
+IMG_DIR = 'C:/Users/CSE124/Desktop/data/'
+
+for img in os.listdir(IMG_DIR):
+        #print(img)
+        p = re.compile('(\d+(\.\d+)*)')  #img name 중 실수 문자열 검색 
+        result = p.findall(img)
+        
+        fish_len=result[2][0]
+        fish_dis=result[3][0]
+        #print(fish_len,fish_dis)
+        img_array = cv2.imread(os.path.join(IMG_DIR,img), cv2.COLOR_BGR2GRAY)
+        #plt.imshow(pixels)
+        #plt.show()
+        value = np.asarray(img_array, dtype=np.int)
+        value = value.flatten()
+        print(value)
+        with open('C:/Users/CSE124/Desktop/output.csv', 'a', encoding="UTF-8") as f:
+            writer = csv.writer(f, delimiter=";")
+            writer.writerow(value)
