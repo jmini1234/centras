f007e74ac221a53b56a676789b0d9df00c328b35
jmini1234 <37237145+jmini1234@users.noreply.github.com>
2019-12-19 20:23:30 +0900

    csv_to_jpg_distance_only

--- Study/Soyeon/csv_to_jpg.py
+++ Study/Soyeon/csv_to_jpg.py
@@ -1,24 +1,11 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import csv
-import cv2
-import os
-
-filename='C:/Users/CSE_125-2/Desktop/output.csv'
-savepath='C:/Users/CSE_125-2/Desktop/test/'
-
-with open(filename) as f:
-    reader = csv.reader(f, delimiter=';')
-    
     isLabel=True
     fish_len=""
-    fish_dis=""
     savename=""
+    
     for row in reader:
         if isLabel:
-            fish_len, fish_dis = row
-            savename=str(str(fish_len)+"_"+str(fish_dis)+".jpg")
+            fish_len = row
+            savename=str(str(fish_len)+".jpg")
             isLabel=False
         else:
             value = np.asarray(row, dtype=np.int)
