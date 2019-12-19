40ee846ff128a82c51c1de07a4f3fa096cbf180c
Jyw526 <55542627+Jyw526@users.noreply.github.com>
2019-12-19 21:35:54 +0900

    aug코드 수정

--- Study/Soyeon/aug_code.py
+++ Study/Soyeon/aug_code.py
@@ -1,35 +1,23 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-import os, glob
-import numpy as np
 
-data_aug_gen=ImageDataGenerator(
-    rescale=1./240
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.5,
-    zoom_range=0.5,
-    horizontal_flip=True,
-    vertical_flip=True,
-    cannel_shift_range=0.5,
-    brightness_range=[0.2,1.0]
-    fill_mode='nearest'
-    )
-print(data_aug_gen)
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
 
-for categoty in categories:
-    image_dir=base_dir+category
-    files=glob.glob(image_dir+"/*.jpg")
-    print(categoty, " 파일 길이 : ", len(files))
-    
-    for image in files:
-        img=load_img(image)
-        print(img)
-        x=img_to_array(img)
-        x=x.reshape((1,)+x.shape)
-        i=0
-        name=imge.split('\\')[1].split('.')[0]
-        for batch in data_aug_gen.flow(x, batch_size=1, save_to_dir="C:/Users/CSE_125-2/Desktop/test"):
-            i+=1
-            if i>3:
-                break
-        
+img = load_img('30.jpg')  # PIL 이미지
+x = img_to_array(img)  # (3, 150, 150) 크기의 NumPy 배열
+x = x.reshape((1,) + x.shape)  # (1, 3, 150, 150) 크기의 NumPy 배열
+
+# 아래 .flow() 함수는 임의 변환된 이미지를 배치 단위로 생성해서
+# 지정된 `preview/` 폴더에 저장합니다.
+i = 0
+for batch in datagen.flow(x, batch_size=1,
+                          save_to_dir='preview', save_prefix='30', save_format='jpg'):
+    i += 1
+    if i > 20:
+        break  # 이미지 20장을 생성하고 마칩니다
