fb5a0a6b05dbe017ee20d0b1086d60c276b3a0a6
soyeon Park <sypark9646@gmail.com>
2019-12-19 16:51:40 +0900

    Update csv_to_jpg.py

--- Study/Soyeon/csv_to_jpg.py
+++ Study/Soyeon/csv_to_jpg.py
@@ -1,8 +1,32 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-df=pd.read_csv('C:/Users/CSE_125-2/Desktop/output.csv', sep=';',header=None)
-pixels = np.array(df).reshape(240,240,3)
+import csv
+import cv2
+import os
 
-plt.imshow(pixels)
-plt.show()
+filename='C:/Users/CSE_125-2/Desktop/output.csv'
+savepath='C:/Users/CSE_125-2/Desktop/test/'
+
+with open(filename) as f:
+    reader = csv.reader(f, delimiter=';')
+    
+    isLabel=True
+    fish_len=""
+    fish_dis=""
+    savename=""
+    for row in reader:
+        if isLabel:
+            fish_len, fish_dis = row
+            savename=str(str(fish_len)+"_"+str(fish_dis)+".jpg")
+            isLabel=False
+        else:
+            value = np.asarray(row, dtype=np.int)
+            value = value.flatten()
+            pixels = np.array(value).reshape(240,240,3)
+            cv2.imwrite(os.path.join(savepath,savename), pixels)
+            isLabel=True
+        
+# pixels = np.array(df).reshape(240,240,3)
+# plt.imshow(pixels)
+# plt.show()
